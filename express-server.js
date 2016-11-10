52973897e72202a4a6346ad79b2118e630b841ee
csawala <chris.sawala@gmail.com>
2016-11-10 01:30:01 +0000

    updated with completed day 2 requireqs

--- express-server.js
+++ express-server.js
@@ -10,10 +10,17 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 
 // =======================================================
+// ==================== DECLARATIONS =====================
 // =======================================================
-function generateRandomString() {
-  let
-  fromCharCode
+const generateRandomString = function() {
+  let randStr = ''
+  // I reallllllllly dislike this method, but it seems the cleanest to make sense of
+  const randOptions = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 6; i++){
+    randStr += randOptions.charAt(Math.floor(Math.random() * 61) + 1)
+  }
+
+  return randStr
 }
 
 const urlDatabase = {
@@ -21,8 +28,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+// =======================================================
+// ==================== APP STUFF ========================
+// =======================================================
+
 app.get("/", (req, res) => {
-  res.end("Hello!")
+  res.redirect("/urls")
 })
 
 app.get("/urls", (req, res) => {
@@ -34,6 +45,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -42,17 +57,27 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars)
 })
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
-})
-
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body)  // debug statement to see POST parameters
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect("/urls/" + shortURL)
 })
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n")
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL)
+})
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  console.log(req.body)  // debug statement to see POST parameters
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect("/urls")
+})
+
+app.post("/urls/:shortURL/delete", (req, res) =>{
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
 })
 
 app.listen(PORT, () => {
